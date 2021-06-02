3ca3e06c61a6a8e2cd76eb5eec12de0d563d13cc
Rafael Oliveira <rafaelcardosooliveira0@gmail.com>
2021-06-02 13:55:21 +0100

    matplot image generator fixed

--- iperf_plotter.py
+++ iperf_plotter.py
@@ -25,6 +25,7 @@ end_time = [int(item['end']) for item in time_intervals_sum]
 total_bytes = [item['bytes'] / 1000000 for item in time_intervals_sum]
 bits_per_second = [item['bits_per_second'] / 1000000 for item in time_intervals_sum]
 
+plt.figure(1)
 plt.title('Throughput')
 plt.subplots_adjust(bottom=0.2)
 plt.plot(end_time, bits_per_second)
@@ -32,6 +33,7 @@ plt.xlabel(f"Time (s)\n\n{date.strftime('%d-%m-%y %H:%M')}")
 plt.ylabel('Mbits/s')
 plt.savefig(f'{output_file_name}_throughput.png')
 
+plt.figure(2)
 plt.title('Total megabytes transferred')
 plt.subplots_adjust(bottom=0.2)
 plt.plot(end_time, total_bytes)
